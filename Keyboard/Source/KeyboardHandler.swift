9b88d65fa88d8e7366ef65cbec0cdf70b38f6558
Sergey Laschuk <slaschuk@shakuro.com>
2018-03-27 14:39:23 +0300

    inu-264-12-[added ability ignore keyboard notifications while app is in background; improved comments]

--- Keyboard/Source/KeyboardHandler.swift
+++ Keyboard/Source/KeyboardHandler.swift
@@ -4,44 +4,52 @@
 //
 
 import UIKit
-
+//TODO: update documentation
 /**
  Utility to handle keyboard appearing & disapearing "in one line".
  */
 public class KeyboardHandler {
-
-    private enum Constant {
-        static let defaultAnimationDuration: TimeInterval = 0.25
-    }
 
     /**
      if `false` notifications about keyboard changes will be skipped.
      Default value is `false`.
      */
     public var isActive: Bool = false
+    /**
+     if `true` changes of keyboard will be handled even if application is in background (resigned active)
+     Default value is `false`.
+     */
+    public var allowChangesInBackground: Bool = false
 
+    private var isAppInBackground: Bool = false
     private var heightDidChange: (CGFloat, TimeInterval) -> Void
     private var observerTokens: [NSObjectProtocol]
 
     // MARK: - Initialization
 
     /**
-     - parameter aHeightDidChange - block, will be called on main thread
+     - Parameters:
+        - aHeightDidChange: block, will be called on main thread
+        - newHeightValue: new height of the keyboard frame assuming keyboard is sticking to the bottom of the screen
+        - animationDuration: duration of the "keyboard hide/show" animation
      */
     public init(heightDidChange aHeightDidChange: @escaping (_ newHeightValue: CGFloat, _ animationDuration: TimeInterval) -> Void) {
         heightDidChange = aHeightDidChange
         observerTokens = []
 
         let center: NotificationCenter = NotificationCenter.default
-        let willShowKeyboardObserverToken = center.addObserver(forName: NSNotification.Name.UIKeyboardWillShow, object: nil, queue: nil, using: { (notification) in
+        let willEnterForegroundToken = center.addObserver(forName: NSNotification.Name.UIApplicationWillEnterForeground, object: nil, queue: nil, using: { (notification) in
+            self.isAppInBackground = false
+        })
+        observerTokens.append(willEnterForegroundToken)
+        let willResignActiveToken = center.addObserver(forName: NSNotification.Name.UIApplicationWillResignActive, object: nil, queue: nil, using: { (notification) in
+            self.isAppInBackground = true
+        })
+        observerTokens.append(willResignActiveToken)
+        let wiilChangeFrameToken = center.addObserver(forName: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil, queue: nil, using: { (notification) in
             self.processKeyboardNotification(notification)
         })
-        observerTokens.append(willShowKeyboardObserverToken)
-
-        let willHideKeyboardObserverToken = center.addObserver(forName: NSNotification.Name.UIKeyboardWillHide, object: nil, queue: nil, using: { (notification) in
-            self.processKeyboardNotification(notification)
-        })
-        observerTokens.append(willHideKeyboardObserverToken)
+        observerTokens.append(wiilChangeFrameToken)
     }
 
     deinit {
@@ -55,13 +63,13 @@ public class KeyboardHandler {
 
     private func processKeyboardNotification(_ notification: Notification) {
         if isActive,
-            let duration = (notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.floatValue,
+            (!isAppInBackground || allowChangesInBackground),
+            let duration = (notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue,
             let keyboardFrame = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            let timeInterval = (duration > 0) ? TimeInterval(duration) : Constant.defaultAnimationDuration
             let screenSize: CGRect = UIScreen.main.bounds
             let height = screenSize.height - keyboardFrame.origin.y
             DispatchQueue.main.async(execute: {
-                self.heightDidChange(height, timeInterval)
+                self.heightDidChange(height, TimeInterval(duration))
             })
         }
     }
