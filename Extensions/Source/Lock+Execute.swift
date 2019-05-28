b5d77937e6e5d0d875bd13257085e0595477fe5f
Vlad <vonipchenko@shakuro.com>
2019-05-28 09:25:06 +0300

    INU-264-44-[improve lock.execute() v2]

--- Extensions/Source/Lock+Execute.swift
+++ Extensions/Source/Lock+Execute.swift
@@ -5,57 +5,10 @@
 
 import Foundation
 
-public protocol ExecuteSyncProtocol {
-    func execute<ResultType>(_ closure: () -> ResultType) -> ResultType
-    func execute<ResultType>(_ closure: () throws -> ResultType) throws -> ResultType
-}
-
-extension NSLock: ExecuteSyncProtocol {
-
-    public func execute<ResultType>(_ closure: () -> ResultType) -> ResultType {
-        let result: ResultType
+extension NSLocking {
+    public func execute<ResultType>(_ closure: () throws -> ResultType) rethrows -> ResultType {
         lock()
-        result = closure()
-        unlock()
-        return result
+        defer { unlock() }
+        return try closure()
     }
-
-    public func execute<ResultType>(_ closure: () throws -> ResultType) throws -> ResultType {
-        let result: ResultType
-        lock()
-        do {
-            result = try closure()
-            unlock()
-            return result
-        } catch let error {
-            unlock()
-            throw error
-        }
-    }
-
-}
-
-extension NSRecursiveLock: ExecuteSyncProtocol {
-
-    public func execute<ResultType>(_ closure: () -> ResultType) -> ResultType {
-        let result: ResultType
-        lock()
-        result = closure()
-        unlock()
-        return result
-    }
-
-    public func execute<ResultType>(_ closure: () throws -> ResultType) throws -> ResultType {
-        let result: ResultType
-        lock()
-        do {
-            result = try closure()
-            unlock()
-            return result
-        } catch let error {
-            unlock()
-            throw error
-        }
-    }
-
 }
