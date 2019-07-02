b2c04c419f36e8ca35a65d829eafec3a8974cbf0
Sergey Laschuk <slaschuk@shakuro.com>
2019-07-02 11:58:26 +0300

    INU-264-57-[quick fix for declaration of tryBlock()]

--- Exceptions/Source/ExceptionCatcher.h
+++ Exceptions/Source/ExceptionCatcher.h
@@ -4,7 +4,7 @@
 
 #import <Foundation/Foundation.h>
 
-NS_INLINE NSException * _Nullable public tryBlock(void(^_Nonnull block)(void)) {
+NS_INLINE NSException * _Nullable tryBlock(void(^_Nonnull block)(void)) {
     @try {
         block();
     }
