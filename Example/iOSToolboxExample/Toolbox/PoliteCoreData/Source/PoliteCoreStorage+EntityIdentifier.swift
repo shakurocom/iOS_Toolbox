9e519ffefafc4961deb45447fb1dee94eebe0afe
Vlad <vonipchenko@shakuro.com>
2019-07-24 15:54:35 +0300

    INU-267-[PoliteCoreStorage+EntityIdentifier documentation]

--- Example/iOSToolboxExample/Toolbox/PoliteCoreData/Source/PoliteCoreStorage+EntityIdentifier.swift
+++ Example/iOSToolboxExample/Toolbox/PoliteCoreData/Source/PoliteCoreStorage+EntityIdentifier.swift
@@ -17,6 +17,7 @@ public extension NSPredicate {
     ///   - format: The format string for additional predicate
     ///   - argumentArray: The arguments for additional predicate
     /// - Returns: The compound predicate "(identifierKey = identifier) AND (additional predicate format)"
+    /// - Tag: objectWithIDPredicate
     class func objectWithIDPredicate(_ identifier: PredicateConvertible,
                                      identifierKey: String = "identifier",
                                      andPredicateFormat format: String? = nil,
@@ -31,6 +32,17 @@ public extension NSPredicate {
 }
 
 public extension PoliteCoreStorage {
+
+    /// Finds first entity that matches identifier and additional predicate, or creates new one if no entity found
+    ///
+    /// - Parameters:
+    ///   - entityType: A type of entity to find
+    ///   - identifier: The identifier of entity, an object that adopts [PredicateConvertible](x-source-tag://PredicateConvertible)
+    ///   - context: NSManagedObjectContext where entity should be find
+    ///   - format: Format for additional predicate. See [objectWithIDPredicate](x-source-tag://PredicateConvertible)
+    ///   - argumentArray: Array of arguments for additional predicate. See [objectWithIDPredicate](x-source-tag://PredicateConvertible)
+    /// - Returns: First found or created entity, never returns nil
+    /// - Tag: findFirstByIdOrCreate
     func findFirstByIdOrCreate<T: NSManagedObject>(_ entityType: T.Type,
                                                    identifier: PredicateConvertible,
                                                    inContext context: NSManagedObjectContext,
@@ -38,6 +50,17 @@ public extension PoliteCoreStorage {
                                                    argumentArray: [Any]? = nil) -> T {
         return findFirstOrCreate(entityType, withPredicate: NSPredicate.objectWithIDPredicate(identifier, andPredicateFormat: format, argumentArray: argumentArray), inContext: context)
     }
+
+
+    /// Finds first entity that matches identifier and additional predicate
+    /// See also [findFirstByIdOrCreate](x-source-tag://PredicateConvertible) for more info
+    /// - Parameters:
+    ///   - entityType: A type of entity to find
+    ///   - identifier: The identifier of entity, an object that adopts [PredicateConvertible](x-source-tag://PredicateConvertible)
+    ///   - context: NSManagedObjectContext where entity should be find
+    ///   - format: Format for additional predicate. See [objectWithIDPredicate](x-source-tag://PredicateConvertible)
+    ///   - argumentArray: Array of arguments for additional predicate. See [objectWithIDPredicate](x-source-tag://PredicateConvertible)
+    /// - Returns: First found entity or nil.
     func findFirstById<T: NSManagedObject>(_ entityType: T.Type,
                                            identifier: PredicateConvertible,
                                            inContext context: NSManagedObjectContext,
