acdff14e79dde64cf7c9e9d1d637c4f4c748ba08
Vlad <vonipchenko@shakuro.com>
2019-08-21 10:05:11 +0300

    INU-267-[minor refactoring]

--- Extensions/Tests/ShortNumberFormatterTest.swift
+++ Extensions/Tests/ShortNumberFormatterTest.swift
@@ -16,28 +16,29 @@ class ShortNumberFormatterTest: XCTestCase {
 
     func testFormatter() {
         let formatter: ShortNumberFormatter = ShortNumberFormatter()
-        let results: [String] = ["100",
-                                 "1k",
-                                 "10k",
-                                 "100k",
-                                 "1M",
-                                 "10M",
-                                 "100M",
-                                 "1G",
-                                 "10G",
-                                 "100G",
-                                 "1T",
-                                 "10T",
-                                 "100T",
-                                 "1P",
-                                 "10P",
-                                 "100P",
-                                 "1E",
-                                 "10E"]
-        for (index, exponent) in (2...19).enumerated() {
-            let value = pow(10, Double(exponent))
+        // exponent -> result string value
+        let testSample: [Double: String] = [2: "100",
+                                            3: "1k",
+                                            4: "10k",
+                                            5: "100k",
+                                            6: "1M",
+                                            7: "10M",
+                                            8: "100M",
+                                            9: "1G",
+                                            10: "10G",
+                                            11: "100G",
+                                            12: "1T",
+                                            13: "10T",
+                                            14: "100T",
+                                            15: "1P",
+                                            16: "10P",
+                                            17: "100P",
+                                            18: "1E",
+                                            19: "10E"]
+        testSample.forEach { (entry) in
+            let value = pow(10, entry.0)
             let formattedValue = formatter.string(for: value)
-            XCTAssertEqual(results[index], formattedValue)
+            XCTAssertEqual(entry.1, formattedValue)
         }
     }
 
