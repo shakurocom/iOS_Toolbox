70d92ee22c95278b007d6e702c77373c1154e095
Vlad <vonipchenko@shakuro.com>
2019-08-21 09:49:00 +0300

    INU-267-[explicit public]

--- Extensions/Source/CALayer+Transition.swift
+++ Extensions/Source/CALayer+Transition.swift
@@ -5,19 +5,19 @@
 
 import UIKit
 
-public extension CALayer {
+extension CALayer {
 
-    enum TransitionAnimationConstant {
+    public enum TransitionAnimationConstant {
         public static let animationKey: String = "custom_layer_transition"
         public static let defaultTransitionType: CATransitionType = .fade
         public static let defaultDuration: CFTimeInterval = 0.15
         public static let defaultTimingFunction: CAMediaTimingFunctionName = .easeInEaseOut
     }
 
-    func addTransitionAnimation(type: CATransitionType = TransitionAnimationConstant.defaultTransitionType,
-                                subType: CATransitionSubtype? = nil,
-                                duration: CFTimeInterval = TransitionAnimationConstant.defaultDuration,
-                                timingFunctionName: CAMediaTimingFunctionName = TransitionAnimationConstant.defaultTimingFunction) {
+    public func addTransitionAnimation(type: CATransitionType = TransitionAnimationConstant.defaultTransitionType,
+                                       subType: CATransitionSubtype? = nil,
+                                       duration: CFTimeInterval = TransitionAnimationConstant.defaultDuration,
+                                       timingFunctionName: CAMediaTimingFunctionName = TransitionAnimationConstant.defaultTimingFunction) {
         let transition: CATransition = CATransition()
         transition.duration = duration
         transition.timingFunction = CAMediaTimingFunction(name: timingFunctionName)
